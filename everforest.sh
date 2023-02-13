5768729c66b13c252340465c639d223f409bb6ef
Jason Halliday <80599443+OGJmon@users.noreply.github.com>
2023-02-13 13:16:03 -0500

    Add aliases with script

--- everforest.sh
+++ everforest.sh
@@ -16,4 +16,20 @@ cp everforest/doc/everforest.txt ~/.vim/doc/
 
 cp .vimrc ~/.vimrc
 
- 
+cd ~/
+if test -f ".bash_aliases"; 
+then
+    
+    if ! grep -Fxq "alias wvr" ".bash_aliases"
+    then
+        echo "alias wvr='ssh -p 2131 ece574-15@weaver-lab.eece.maine.edu'" >> ~/.bash_aliases
+    fi
+
+    if ! grep -Fxq "alias pi" ".bash_aliases"
+    then
+        echo "alias pi='ssh pi@192.168.0.124'" >> ~/.bash_aliases
+    fi
+else
+    echo "alias wvr='ssh -p 2131 ece574-15@weaver-lab.eece.maine.edu'" >> ~/.bash_aliases
+    echo "alias pi='ssh pi@192.168.0.124'" >> ~/.bash_aliases
+fi
